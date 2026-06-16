293978edabd1c7a5edd25a1dc37f96454f373241
Jordy Pinos <jordypinos3@gmail.com>
2026-06-16 17:00:52 +0200

    fix: Improve treesitter configuration using the new API

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  lazy = false,
   build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  main = "nvim-treesitter.config",
-  opts = {
-    ensure_installed = { "go", "lua", "vim", "bash", "dart" },
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
-  init = function()
+  config = function()
+    require('nvim-treesitter').install { 'go', 'lua' }
+
     vim.wo.foldmethod = 'expr'
     vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.wo.foldenable = true
     vim.wo.foldlevel = 99
 
     vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'go'},
+      pattern = { 'go' },
       callback = function() vim.treesitter.start() end,
     })
   end
